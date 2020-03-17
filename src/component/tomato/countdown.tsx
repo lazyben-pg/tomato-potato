f58e94d54743581d6df00ce909261084139b9c27
lazzben <458861463@qq.com>
2020-03-17 17:49:55 +0800

    完成 tomatoInput，并优化代码

--- src/component/tomato/countdown.tsx
+++ src/component/tomato/countdown.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import './countdown.scss'
 
 interface ICountdownProps {
   timer: number,
-  finish: () => void
+  finish: () => void,
+  duration: number
 }
 
 interface ICountdownState {
@@ -21,7 +23,7 @@ class Countdown extends React.Component<ICountdownProps, ICountdownState> {
     let timeId: NodeJS.Timeout
     timeId = setInterval(() => {
       this.setState({ timer: this.state.timer - 1000 })
-      this.state.timer < 1000 && this.finish(timeId)
+      this.state.timer < 100 && this.finish(timeId)
     }, 1000)
   }
 
@@ -34,8 +36,12 @@ class Countdown extends React.Component<ICountdownProps, ICountdownState> {
     const min = Math.floor(this.state.timer / 1000 / 60)
     const sec = Math.floor(this.state.timer / 1000 % 60)
     const time = `${min >= 10 ? min : '0' + min}:${sec >= 10 ? sec : '0' + sec}`
+    const width = (1 - this.state.timer / this.props.duration) * 100 + '%'
     return (
-      <div>{time}</div>
+      <div id="countdown">
+        <span className="time">{time}</span>
+        <div className="progress" style={{ width: `${width}` }} />
+      </div>
     )
   }
 }
--- src/component/tomato/tomatoinput.tsx
+++ src/component/tomato/tomatoinput.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Button, Input } from 'antd'
+import { Button, Input, Popconfirm } from 'antd'
 import Countdown from './countdown'
 import axios from '../../config/axios'
+import './tomatoinput.scss'
+import { CloseCircleOutlined } from '@ant-design/icons'
 
 interface ITomatoinputProps {
   addTomato: (params: any) => any,
@@ -10,29 +12,31 @@ interface ITomatoinputProps {
 }
 
 interface ITomatoinputState {
-  description: string
+  description: string,
+  duration: number
 }
 
 class Tomatoinput extends React.Component<ITomatoinputProps, ITomatoinputState> {
   constructor(props: any) {
     super(props)
     this.state = {
-      description: ''
+      description: '',
+      duration: 60 * 1000
     }
   }
 
-  componentDidMount() {
-    console.log(this.props.unfishedTomato)
-  }
-
   addTomato = () => {
-    this.props.addTomato({ 'duration': 60 * 1000 })
+    this.props.addTomato({ 'duration': this.state.duration })
   }
 
   updateTomato = async (params: any) => {
     await axios.put(`tomatoes/${this.props.unfishedTomato.id}`, params)
       .then(res => { this.props.updateTomato(res.data.resource) })
       .catch(err => { throw new Error(err) })
+  }
+
+  abortTomato = () => {
+    this.updateTomato({ aborted: true })
   }
 
   addDescription = () => {
@@ -43,38 +47,68 @@ class Tomatoinput extends React.Component<ITomatoinputProps, ITomatoinputState> 
     this.setState({ 'description': '' })
   }
 
-  finish = () => {
+  countDownFinish = () => {                    //for countdown component
     this.forceUpdate()
   }
 
-  render() {
+  confirm = () => {
+    return (
+      <Popconfirm
+        title="你确定要终止这项任务吗？"
+        onConfirm={this.abortTomato}
+        okText="是"
+        cancelText="否"
+      >
+        <CloseCircleOutlined style={{ position: 'absolute', right: '-5px', top: '-5px', background: '#fff' }} />
+      </Popconfirm>
+    )
+  }
+
+  showStartBtn = () => {
+    return <Button className="starttomatobtn" onClick={this.addTomato}>开始番茄</Button>
+  }
+
+  showInputTomato = () => {
+    return (
+      <div className="inputwrapper">
+        <Input placeholder="请输入你刚刚完成的任务"
+          value={this.state.description}
+          onChange={e => this.setState({ description: e.target.value })}
+          onPressEnter={this.addDescription}
+        />
+        {this.confirm()}
+      </div>
+    )
+  }
+
+  showCountDown = (startedAt: number, duration: number, now: number) => {
+    return (
+      <div className="countdownwrapper">
+        <Countdown timer={duration - now + startedAt} finish={this.countDownFinish} duration={this.state.duration} />
+        {this.confirm()}
+      </div>
+    )
+  }
+
+  showWhich = () => {
     let html = <div />
     if (this.props.unfishedTomato === undefined) {
-      html = <Button onClick={this.addTomato}>开始番茄</Button>
+      html = this.showStartBtn()
     } else {
       const startedAt = Date.parse(this.props.unfishedTomato.started_at)
       const duration = this.props.unfishedTomato.duration
       const now = new Date().getTime()
-      if (now - startedAt > duration) {
-        html = (
-          <div className="inputwrapper">
-            <Input placeholder="请输入你刚刚完成的任务"
-              value={this.state.description}
-              onChange={e => this.setState({ description: e.target.value })}
-              onPressEnter={this.addDescription}
-            />
-          </div>)
-      } else {
-        html = (
-          <div className="countdownwrapper">
-            <Countdown timer={duration - now + startedAt} finish={this.finish} />
-          </div>
-        )
-      }
+      html = now - startedAt > duration ?
+        this.showInputTomato() :
+        this.showCountDown(startedAt, duration, now)
     }
+    return html
+  }
+
+  render() {
     return (
       <div id="tomatoinput">
-        {html}
+        {this.showWhich()}
       </div>
     )
   }
