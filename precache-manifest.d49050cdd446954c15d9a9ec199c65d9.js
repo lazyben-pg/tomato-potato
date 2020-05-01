a74618d10b882b063313c90d1e03918201bc1d6a
lazzben <458861463@qq.com>
2020-05-01 21:01:20 +0800

    Updates

---
+++ precache-manifest.d49050cdd446954c15d9a9ec199c65d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe7f4bb1d41c7f05ccd40c717ca9b4b",
+    "url": "/tomato-potato/index.html"
+  },
+  {
+    "revision": "08feb6c0cafba6de763d",
+    "url": "/tomato-potato/static/css/2.ca55a93c.chunk.css"
+  },
+  {
+    "revision": "d638a10bc4bf2cf16977",
+    "url": "/tomato-potato/static/css/main.9d441407.chunk.css"
+  },
+  {
+    "revision": "08feb6c0cafba6de763d",
+    "url": "/tomato-potato/static/js/2.edeb40d3.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/tomato-potato/static/js/2.edeb40d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d638a10bc4bf2cf16977",
+    "url": "/tomato-potato/static/js/main.3809c0a2.chunk.js"
+  },
+  {
+    "revision": "503113e675c92d94168c",
+    "url": "/tomato-potato/static/js/runtime-main.3fe66e00.js"
+  }
+]);
